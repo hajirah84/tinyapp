7fad49c184e51325ab51a242b1904917bc6a4934
hajirah84 <mianhajirah@gmail.com>
2024-12-09 00:06:45 -0500

    url shortening part1

--- express_server.js
+++ express_server.js
@@ -2,12 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+function generateRandomString() {}
+
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,8 +33,19 @@ app.listen(PORT, () => {
     res.render("urls_index", templateVars);
   });
   
+  app.get("/urls/new", (req, res) => {
+    res.render("urls_new");
+  });
+
   app.get("/urls/:id", (req, res) => {
    const urlId = req.params.id; 
     const templateVars = { id: urlId, longURL: urlDatabase[urlId] };
     res.render("urls_show", templateVars);
   });
+ 
+  app.post("/urls", (req, res) => {
+    console.log(req.body); // Log the POST request body to the console
+    res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  });
+
+
