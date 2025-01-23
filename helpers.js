1c2987e458999282615cf8aa371610a0ed66cdaf
hajirah84 <mianhajirah@gmail.com>
2025-01-22 19:56:02 -0500

    added comments to helper file

--- helpers.js
+++ helpers.js
@@ -1,11 +1,11 @@
-const getUserByEmail = function(email, database) {
+const getUserByEmail = function(email, database) { //iterate through database to find a user with a matching email
     for (const userId in database) {
       const user = database[userId];
       if (user.email === email) {
         return user; 
       }
     }
-    return null; 
+    return null; //return null if no user with the provided email is found
   };
   
   module.exports = { getUserByEmail };
